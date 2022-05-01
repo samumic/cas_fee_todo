649a7fddd7dab3a9ce0e789f9a748bd76c7ae716
samirmumic <sam.mumic@gmail.com>
2022-05-01 12:39:10 +0200

    feat(INIT): initial setup

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('source/public'));
 
 app.listen(port, () => {
     // eslint-disable-next-line no-console
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`cas-fee-todo app listening at http://localhost:${port}`);
 });
